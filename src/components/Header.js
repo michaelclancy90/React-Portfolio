e960dcda8dcefdbcfd73307249ca322a918684c2
michaelclancy90 <michaelclancy@hotmail.co.uk>
2022-10-06 20:01:46 +1100

    header file updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,54 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavLink } from 'react-router-dom';
 
-export default function Header(props) {
-  return <div></div>;
+export default function Header() {
+  let headerActiveStyle = {
+    textDecoration: 'underline wavy blue 5px',
+  };
+  return (
+    <div>
+      <ul>
+        <li>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? 'headerActiveStyle' : undefined
+            }
+            to='/'
+          >
+            About Me
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? 'headerActiveStyle' : undefined
+            }
+            to='/resume'
+          >
+            Resume
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? 'headerActiveStyle' : undefined
+            }
+            to='/portfolio'
+          >
+            Portfolio
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? 'headerActiveStyle' : undefined
+            }
+            to='/contact'
+          >
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
